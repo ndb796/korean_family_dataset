6d65c43abf1817ef532e09a2edf042307b78ab08
Dongbin Na / 나동빈 <ndb796@naver.com>
2022-08-28 14:22:58 +0900

    Create unzip.py

---
+++ unzip.py
@@ -0,0 +1,19 @@
+import os
+import zipfile
+
+
+directories = [
+    './family_dataset/1.Training/labeled',
+    './family_dataset/1.Training/original',
+    './family_dataset/2.Validation/labeled',
+    './family_dataset/2.Validation/original'
+]
+for directory in directories:
+    for file in os.listdir(directory):
+        # 전체 경로(.zip까지)
+        path = os.path.join(directory, file)
+        if path[-4:] == '.zip': # 압축 파일(.zip)인 경우
+            compressed = zipfile.ZipFile(path)
+            # 동일 폴더에 압축 해제
+            compressed.extractall(path[:-4])
+            compressed.close()
